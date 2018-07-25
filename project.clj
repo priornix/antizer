c43ef5bfbbebb14b2ce689d63eff4cace03082e9
Kevin Jiang <kevin.jiang@lexisnexis.com>
2018-07-25 08:57:45 +0800

    0.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kittenll/antizer "0.5.0"
+(defproject kittenll/antizer "0.5.1"
   :description "Ant Design ClojureScript Wrapper"
   :url "https://github.com/kevindragon/antizer"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [cljsjs/antd "3.7.1-0"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.4"]]
   :codox {:language :clojurescript
           :metadata {:doc/format :markdown}
           :output-path "doc/dist/latest/api"
